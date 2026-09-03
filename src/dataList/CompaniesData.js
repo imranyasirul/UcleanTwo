b7e721d4c486b482e3851ca112cc5e1644410ac3
imranyasirul <imranyasirul@gmail.com>
2026-09-03 12:26:01 +0600

    practice more

---
+++ src/dataList/CompaniesData.js
@@ -0,0 +1,40 @@
+import companiesPic1 from "../assets/companiesPic1.png";
+import companiesPic2 from "../assets/companiesPic2.png";
+import companiesPic3 from "../assets/companiesPic3.png";
+import companiesPic4 from "../assets/companiesPic4.png";
+let CompaniesData = [
+  {
+    id: 0,
+    img: companiesPic1,
+    title: "Professional Team",
+    description: `Our trained, insured cleaners ensure
+professional, trusted service and
+impeccable results.`,
+  },
+  {
+    id: 0,
+    img: companiesPic2,
+    title: "Professional Team",
+    description: `Our trained, insured cleaners ensure
+professional, trusted service and
+impeccable results.`,
+  },
+  {
+    id: 0,
+    img: companiesPic3,
+    title: "Professional Team",
+    description: `Our trained, insured cleaners ensure
+professional, trusted service and
+impeccable results.`,
+  },
+  {
+    id: 0,
+    img: companiesPic4,
+    title: "Professional Team",
+    description: `Our trained, insured cleaners ensure
+professional, trusted service and
+impeccable results.`,
+  },
+];
+
+export default CompaniesData;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,6 +13,7 @@ import aboutStar from "../assets/Star.png";
 import SubHeading from "../components/common/SubHeading";
 import HeadingTwo from "../components/common/HeadingTwo";
 import serviceData from "../dataList/ServiceData";
+import CompaniesData from "../dataList/CompaniesData";
 
 const Home = () => {
   return (
@@ -88,7 +89,7 @@ const Home = () => {
           </div>
 
           {/* =======Service Start======= */}
-          <div className="serviceHeading text-center flex justify-center mt-20 flex-col items-center">
+          <div className="serviceHeading text-center flex justify-center mt-20 flex-col items-center pb-25">
             <SubHeading text="Our Services" />
             <HeadingTwo className="mt-3.5 mb-8" text="Our Cleaning Services" />
             <ComDes
@@ -109,13 +110,15 @@ const Home = () => {
       {/* ======About Uclean End===== */}
 
       {/* ======Services Start===== */}
-      <section className="pb-20 relative bottom-50">
+      <section className=" relative bottom-30">
         <Container>
-          <div className="serviceWrap flex flex-wrap py-25 ">
+          <div className="serviceWrap flex flex-wrap ">
             {serviceData.map((item) => (
-              <div key={item.id} className="w-[31%]">
+              <div key={item.id} className="w-102.5">
                 <Image source={item.img} alt={item.title} />
-                <h3 className=" my-3 text-5 font-semibold font-quicksand text-[#161C2D]">{item.title}</h3>
+                <h3 className=" my-3 text-5 font-semibold font-quicksand text-[#161C2D]">
+                  {item.title}
+                </h3>
                 <ComDes className=" text-[#707070]" text={item.description} />
               </div>
             ))}
@@ -124,8 +127,61 @@ const Home = () => {
       </section>
       {/* ======Services End===== */}
 
-      {/* ====== Start===== */}
-      {/* ====== End===== */}
+      {/* ======Companies Start===== */}
+      <section className="py-20 bg-[#F8F9FA]">
+        <Container>
+          <div className="companiesWrap flex justify-between">
+            {CompaniesData.map((item) => (
+              <div key={item.id} className="flex flex-col items-center">
+                <Image source={item.img} alt={item.title} />
+                <h3 className=" my-3 text-5 font-semibold font-quicksand text-[#161C2D]">
+                  {item.title}
+                </h3>
+                <ComDes className=" text-center" text={item.description} />
+              </div>
+            ))}
+          </div>
+          {/* ======Counter start */}
+          <div className="counter py-12 bg-[#FEE74A] px-30 mt-20">
+            <div className="counterWrap flex justify-between">
+              <div className="countUser">
+                <h4 className="font-semibold font-quicksand leading-10 text-[40px] text-[#161C2D]">
+                  65250 +
+                </h4>
+                <p className="text-[15px] text-[#707070] font-normal font-open-sans">
+                  Houses Cleaned
+                </p>
+              </div>
+              <div className="countUser">
+                <h4 className="font-semibold font-quicksand leading-10 text-[40px] text-[#161C2D]">
+                  23160 +
+                </h4>
+                <p className="text-[15px] text-[#707070] font-normal font-open-sans">
+                  Happy Customers
+                </p>
+              </div>
+              <div className="countUser">
+                <h4 className="font-semibold font-quicksand leading-10 text-[40px] text-[#161C2D]">
+                  150 +
+                </h4>
+                <p className="text-[15px] text-[#707070] font-normal font-open-sans">
+                  Experienced Cleaners
+                </p>
+              </div>
+              <div className="countUser">
+                <h4 className="font-semibold font-quicksand leading-10 text-[40px] text-[#161C2D]">
+                  20 +
+                </h4>
+                <p className="text-[15px] text-[#707070] font-normal font-open-sans">
+                  Years of Exeperience
+                </p>
+              </div>
+            </div>
+          </div>
+          {/* ======Counter End */}
+        </Container>
+      </section>
+      {/* ======Companies End===== */}
 
       {/* ====== Start===== */}
       {/* ====== End===== */}
